856fb1b98d64848c66c398b94bbd0d5a23accee3
Riz <rizwana.akmal@hotmail.com>
2024-03-21 18:57:08 +0000

    Update route.ts

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -3,7 +3,7 @@ import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
 const ip = request.ip;
-const city = request.geo;
+const city = request.geo?.city;
 console.log("ip ", ip);
 console.log("city ", city);
 return Response.json({ ip, city })
